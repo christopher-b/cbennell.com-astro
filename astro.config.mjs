0797c42d712aad238103185ff483b243a3a009a7
Christopher Bennell <cbennell@hey.com>
2026-04-26 23:30:27 -0400

    Syntax highlights, editor config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,30 @@
 // @ts-check
 
+import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, fontProviders } from "astro/config";
+import { pluginCodeCaption } from "@fujocoded/expressive-code-caption";
 import { readingTime } from "./src/plugins/reading-time.mjs";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cbennell.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    expressiveCode({
+      styleOverrides: {
+        codeFontSize: "0.75rem",
+        frames: {
+          frameBoxShadowCssValue: "none",
+        },
+      },
+      plugins: [pluginCodeCaption()],
+      themes: ["andromeeda", "catppuccin-latte"],
+      extractFileNameFromCode: false,
+    }),
+    mdx(),
+    sitemap(),
+  ],
   markdown: {
     remarkPlugins: [readingTime],
   },
